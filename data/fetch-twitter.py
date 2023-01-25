51f660ad07223522d7114b159fe42245aae0980c
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2023-01-25 22:05:45 +0100

    add twitter tweet fetching class

---
+++ data/fetch-twitter.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+import requests
+import tweepy 
+import json
+import os
+import re
+
+from timeit import default_timer as timer
+
+import nltk
+import arabic_reshaper
+from bidi.algorithm import get_display
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.express as px
+import plotly.graph_objects as go
+from wordcloud import WordCloud
+
+class TWEETS():
+
+    def __init__():
+
