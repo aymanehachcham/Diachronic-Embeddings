066849af17186de9070eddaf017c78477c3c96a6
Imene KOLLI <53266529+imene-swaan@users.noreply.github.com>
2023-01-29 21:21:40 +0100

    Update reviews.py

--- data/reviews.py
+++ data/reviews.py
@@ -1,31 +1,51 @@
-import ijson
-import zipfile 
+import numpy as np
 import pandas as pd
+import json
 
-with zipfile.ZipFile("Books_5.json.zip", "r") as z:
-
+with open('Books_5.json', 'r') as f:
     i = 0
     data = {}
 
-    with open("Books_5.json", "rb") as f:
-        for record in ijson.items(f, "item"):
+    for line in f:
+        a = json.loads(line)
 
-            reviewTime = record["reviewTime"]
-            asin = record["asin"]
-            reviewerName = record['reviewerName']
-            reviewText = record['reviewText']
-            summary = record['summary']
+        if 'reviewText' not in a.keys():
+            continue
 
-            data[i] = [reviewTime, asin, reviewerName, reviewText, summary]
-            i += 1
+        else:
+            if a['verified'] == False:
+                continue
 
+            elif int(a['reviewTime'].split()[-1]) < 2013:
+                continue
 
-
+            elif i < 5000000:
+                data[i] = json.loads(line)
+                i += 1
+                print(i)
+            else:
+                break
+         
 df = pd.DataFrame.from_dict(data, orient='index')
-
-df.to_csv(index=False)
-
-
-
+df['time'] = pd.to_datetime(df['reviewTime'], infer_datetime_format=True)
+df.drop(['reviewTime', 'reviewerID', 'style', 'reviewerName', 'unixReviewTime', 'vote', 'image', 'summary', 'verified'], axis = 1, inplace= True)
+df['year'] = df['time'].dt.year
 
 
+df_2013 = df[df['year'] == 2013]
+df_2013.to_csv('df_2013.csv')
+
+df_2014 = df[df['year'] == 2014]
+df_2014.to_csv('df_2014.csv')
+
+df_2015 = df[df['year'] == 2015]
+df_2015.to_csv('df_2015.csv')
+
+df_2016 = df[df['year'] == 2016]
+df_2016.to_csv('df_2016.csv')
+
+df_2017 = df[df['year'] == 2017]
+df_2017.to_csv('df_2017.csv')
+
+df_2018 = df[df['year'] == 2018]
+df_2018.to_csv('df_2018.csv')
